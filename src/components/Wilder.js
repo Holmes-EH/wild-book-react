7775ed3cd8a13cb285767e99b08208303a4b9377
Holmes-EH <holmes.samuel@yahoo.fr>
2022-09-15 12:33:30 +0200

    display description from backend

--- src/components/Wilder.js
+++ src/components/Wilder.js
@@ -2,18 +2,13 @@ import blank_profile from '../assets/blank_profile.png'
 import Skill from './Skill'
 
 const Wilder = ({ wilder }) => {
-	const { name, city, skills } = wilder
+	const { name, city, description, skills } = wilder
 	return (
 		<article className='card'>
 			<img src={blank_profile} alt='Jane Doe Profile' />
 			<h3>{name}</h3>
 			<h4>{city}</h4>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-				eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-				enim ad minim veniam, quis nostrud exercitation ullamco laboris
-				nisi ut aliquip ex ea commodo consequat.
-			</p>
+			<p>{description}</p>
 			<h4>Wild Skills</h4>
 			<ul className='skills'>
 				{skills.map((skill, index) => {
